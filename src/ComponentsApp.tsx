6b416bc00bf64b94026b8067b59fdb308d7b0883
emanuelhardwell <emanuelnoe16@gmail.com>
2024-06-25 20:31:16 -0600

    feat(dep): Setup StackNavigator}

--- src/ComponentsApp.tsx
+++ src/ComponentsApp.tsx
@@ -1,9 +1,11 @@
-import {Text, View} from 'react-native';
+import {NavigationContainer} from '@react-navigation/native';
+import 'react-native-gesture-handler';
+import {StackNavigator} from './presentation/navigators/StackNavigator';
 
 export const ComponentsApp = () => {
   return (
-    <View>
-      <Text>ComponentsApp</Text>
-    </View>
+    <NavigationContainer>
+      <StackNavigator />
+    </NavigationContainer>
   );
 };
