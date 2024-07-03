a2f6228fae711a8a84d030acef26b97cd86aab5e
emanuelhardwell <emanuelnoe16@gmail.com>
2024-07-02 21:06:04 -0600

    feat: Update custom component Title

--- src/presentation/components/ui/Title.tsx
+++ src/presentation/components/ui/Title.tsx
@@ -1,5 +1,5 @@
 import {FC} from 'react';
-import {Text} from 'react-native';
+import {StyleProp, Text, TextStyle} from 'react-native';
 import {colors, globalStyles} from '../../../config/theme/theme';
 import {useSafeAreaInsets} from 'react-native-safe-area-context';
 
@@ -7,19 +7,28 @@ interface TitleProps {
   text: string;
   safe?: boolean;
   white?: boolean;
+  style?: StyleProp<TextStyle>;
 }
 
-export const Title: FC<TitleProps> = ({text, safe = false, white = false}) => {
+export const Title: FC<TitleProps> = ({
+  text,
+  safe = false,
+  white = false,
+  style,
+}) => {
   const {top} = useSafeAreaInsets();
 
   return (
     <Text
-      style={{
-        ...globalStyles.title,
-        marginTop: safe ? top : 0,
-        marginBottom: 10,
-        color: white ? 'white' : colors.text,
-      }}>
+      style={[
+        {
+          ...globalStyles.title,
+          marginTop: safe ? top : 0,
+          marginBottom: 10,
+          color: white ? 'white' : colors.text,
+        },
+        style,
+      ]}>
       {text}
     </Text>
   );
