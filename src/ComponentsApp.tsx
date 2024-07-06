c700ba4ad6c2df7ee5505538d1dabf41d6920886
emanuelhardwell <emanuelnoe16@gmail.com>
2024-07-05 23:20:10 -0600

    feat: Update component root ComponentsApp and use the ThemeContext

--- src/ComponentsApp.tsx
+++ src/ComponentsApp.tsx
@@ -1,11 +1,21 @@
 import {NavigationContainer} from '@react-navigation/native';
 import 'react-native-gesture-handler';
 import {StackNavigator} from './presentation/navigators/StackNavigator';
+import {PropsWithChildren} from 'react';
+import {ThemeProvider} from './presentation/context/ThemeContext';
+
+const AppState = ({children}: PropsWithChildren) => {
+  return (
+    <NavigationContainer>
+      <ThemeProvider>{children}</ThemeProvider>
+    </NavigationContainer>
+  );
+};
 
 export const ComponentsApp = () => {
   return (
-    <NavigationContainer>
+    <AppState>
       <StackNavigator />
-    </NavigationContainer>
+    </AppState>
   );
 };
